867536d05897331befad7f2ec44bd788cc5463a1
amitsahni <sahni1987@gmail.com>
2016-09-04 11:02:17 +0530

    Changes in base

--- base/src/main/java/android/base/ui/widget/BaseSwipeRefreshLayout.java
+++ base/src/main/java/android/base/ui/widget/BaseSwipeRefreshLayout.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 /**
@@ -14,6 +17,7 @@ import android.util.AttributeSet;
  */
 public class BaseSwipeRefreshLayout extends SwipeRefreshLayout implements SwipeRefreshLayout.OnRefreshListener {
     private OnRefreshListener onRefreshListener;
+    private ViewGroup container;
 
     @Override
     public void onRefresh() {
@@ -77,4 +81,48 @@ public class BaseSwipeRefreshLayout extends SwipeRefreshLayout implements SwipeR
 
     }
 
+    @Override
+    public boolean canChildScrollUp() {
+        // The swipe refresh layout has 2 children; the circle refresh indicator
+        // and the view container. The container is needed here
+        ViewGroup container = getContainer();
+        if (container == null) {
+            return false;
+        }
+
+        // The container has 2 children; the empty view and the scrollable view.
+        // Use whichever one is visible and test that it can scroll
+        View view = container.getChildAt(0);
+        if (view.getVisibility() != View.VISIBLE) {
+            view = container.getChildAt(1);
+        }
+
+        return ViewCompat.canScrollVertically(view, -1);
+    }
+
+    private ViewGroup getContainer() {
+        // Cache this view
+        if (container != null) {
+            return container;
+        }
+
+        // The container may not be the first view. Need to iterate to find it
+        for (int i = 0; i < getChildCount(); i++) {
+            if (getChildAt(i) instanceof ViewGroup) {
+                container = (ViewGroup) getChildAt(i);
+
+                if (container.getChildCount() != 2) {
+                    throw new RuntimeException("Container must have an empty view and content view");
+                }
+
+                break;
+            }
+        }
+
+        if (container == null) {
+            throw new RuntimeException("Container view not found");
+        }
+
+        return container;
+    }
 }
